4472b271261f4dbedb0a52043d3cf426239d4fb0
Alinur <alinurraiymbekova@gmail.com>
2025-03-18 21:57:50 +0500

    usage of when instead of if-else if

--- src/Main.kt
+++ src/Main.kt
@@ -37,28 +37,29 @@ fun main() {
 //        println("Вы разместили объявление: ")
 //        printFullCarInfo(modelInput, price, year)
 //    }
-    var adNum = 0;
-    while (true) {
-        print("Введите 0 для вызова или 1 для выхода: ")
-        var userEnter = readLine()?.toInt()
-
-        if (userEnter == 1) {
-            break
-        }
-        print("Введите модель автомобиля: ")
-        val modelInput = getModelInfo()
-
-        print("Введите цену автомобиля (тенге): ")
-        val price = getPriceInfo()
-
-        print("Введите год выпуска автомобиля: ")
-        val year = getYearInfo()
-
-        adNum++
-        println("Вы разместили объявление #$adNum: ")
-        printFullCarInfo(modelInput, price, year)
-    }
-
+//    var adNum = 0;
+//    while (true) {
+//        print("Введите 0 для вызова или 1 для выхода: ")
+//        var userEnter = readLine()?.toInt()
+//
+//        if (userEnter == 1) {
+//            break
+//        }
+//        print("Введите модель автомобиля: ")
+//        val modelInput = getModelInfo()
+//
+//        print("Введите цену автомобиля (тенге): ")
+//        val price = getPriceInfo()
+//
+//        print("Введите год выпуска автомобиля: ")
+//        val year = getYearInfo()
+//
+//        adNum++
+//        println("Вы разместили объявление #$adNum: ")
+//        printFullCarInfo(modelInput, price, year)
+//    }
+    val carInfo = getCarInfo()
+    printFullCarInfo(carInfo,)
 }
 
 fun getModelInfo(): String? {
@@ -75,6 +76,7 @@ fun getPriceInfo(): Int? {
         priceString = "0"
     }
     val price = priceString?.toInt()
+
     return price
 }
 
@@ -86,6 +88,39 @@ fun getYearInfo(): Int? {
     val year = yearString?.toInt()
     return year
 }
+
+fun getCarInfo(): String {
+    print("Введите модель автомобиля: ")
+    val model = readLine() ?: "Неизвестная модель"
+
+    print("Введите цену автомобиля: ")
+    val priceInput = readLine() ?: "0"
+    val price = priceInput.toInt()
+
+//    if (price < 0) {
+//        println("Цена не может быть отрицательной! Сбросим в 0.")
+//        return "Модель = $model, Цена = 0 $CURRENCY"
+//    } else if (price > 10_000_000) {
+//        println("Очень высокая цена! Вы уверены?")
+//    }
+    when {
+        price < 0 -> "Цена не может быть отрицательной! Сбросим в 0."
+        price > 10_000_000 -> "Очень высокая цена! Вы уверены?"
+        else -> "Цена подходящая"
+    }
+
+//    if (model.equals("Lexus", ignoreCase = true) && price < 5_000_000) {
+//        println("Для Lexus обычно ожидается цена от 5_000_000!")
+//    }
+//    return "Модель = $model, Цена = $price $CURRENCY"
+
+    when{
+        model.equals("Lexus", ignoreCase = true) && price < 5_000_000 -> "Для Lexus обычно ожидается цена от 5_000_000!"
+        model.equals("VAZ", ignoreCase = true) && price > 2_000_000 -> "Для VAZ обычно ожидается цена ниже 2_000_000!"
+    }
+    return "Модель = $model, Цена = $price $CURRENCY"
+}
+
 
 fun printFullCarInfo(modelInput: String?, price: Int?, year: Int?) {
     val text = """
